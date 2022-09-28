aebed2ffc20afa22458e1d7af21a75ff57748f7b
Allan Mello de Macedo <allanmellom@gmail.com>
2022-09-28 18:50:14 -0300

    !

--- README.md
+++ README.md
@@ -2,8 +2,16 @@
 
 Description. 
 The package package_name is used to:
-	- 
-	-
+	- operacoes_complexas
+		- fatorial
+		- somatoria
+		- potencia
+		- raiz
+	- operacoes_simples
+		- soma
+		- subtracao
+		- divisao
+		- multiplicacao
 
 ## Installation
 
@@ -11,13 +19,6 @@ Use the package manager [pip](https://pip.pypa.io/en/stable/) to install package
 
 ```bash
 pip install package_name
-```
-
-## Usage
-
-```python
-from package_name.module1_name import file1_name
-file1_name.my_function()
 ```
 
 ## Author
--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     description="Pacote criado para o desafio da Geração Tech Unimed BH - Ciência de Dados, na plataforma DIO.",
     long_description=page_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/allanmellom/Projeto_Criacao_de_Pacotes-calculadora_Geracao_Tech_Unimed-BH-Ciencia_de_Dados-DIO"
+    url="https://github.com/allanmellom/Projeto_Criacao_de_Pacotes-calculadora_Geracao_Tech_Unimed-BH-Ciencia_de_Dados-DIO",
     packages=find_packages(),
     install_requires=requirements,
     python_requires='>=3.8',

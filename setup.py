cdaa190b91f4743b4c9efeebe0c614164b68f296
Allan Mello de Macedo <allanmellom@gmail.com>
2022-09-28 18:31:32 -0300

    NEW

--- setup.py
+++ setup.py
@@ -7,14 +7,14 @@ with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
 setup(
-    name="package_name",
+    name="calculadora_simples",
     version="0.0.1",
-    author="my_name",
-    author_email="my_email",
-    description="My short description",
+    author="Allan",
+    author_email="allanmellom@gmail.com",
+    description="Pacote criado para o desafio da Geração Tech Unimed BH - Ciência de Dados, na plataforma DIO.",
     long_description=page_description,
     long_description_content_type="text/markdown",
-    url="my_github_repository_project_link"
+    url="https://github.com/allanmellom/Projeto_Criacao_de_Pacotes-calculadora_Geracao_Tech_Unimed-BH-Ciencia_de_Dados-DIO"
     packages=find_packages(),
     install_requires=requirements,
     python_requires='>=3.8',
